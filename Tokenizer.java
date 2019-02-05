71824b998cec9b76ffcd78ccfed5671297a087ee
Kyle Dewey <kyle.dewey@csun.edu>
2019-02-05 14:05:17 -0800

    end of class 2/5/19

--- Tokenizer.java
+++ Tokenizer.java
@@ -2,15 +2,30 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Tokenizer {
+    //public static Optional<VariableToken>
     public static VariableToken tryTokenizeVariable(char[] input,
                                                     int inputPos) {
-        List<Character> name = new ArrayList<Character>();
+        String name = "";
 
-        if (input[inputPos] is legal first character) {
-            name.add(input[inputPos]);
-            while (inputPos < input.length) {
-                if (input[inputPos] is legal next character) {
-                    
+        if (Character.isLetter(input[inputPos])) {
+            name += input[inputPos];
+            inputPos++;
+            
+            while (inputPos < input.length &&
+                   Character.isLetterOrDigit(input[inputPos])) {
+                name += input[inputPos];
+                inputPos++;
+            }
+        } else {
+            return null;
+        }
+
+        if (name.equals("if")) {
+            return null;
+        } else {
+            return new VariableToken(name);
+        }
+    }
         
     public static List<Token> tokenize(char[] input) {
         List<Token> list = new ArrayList<Token>();
@@ -19,9 +34,11 @@ public class Tokenizer {
         while (inputPos < input.size) {
             if (input[inputPos] == '(') {
                 list.add(new LeftParenToken());
+                inputPos++;
             } else if (input[inputPos] == 'i' &&
-                input[inputPos + 1] == 'f') {
+                       input[inputPos + 1] == 'f') {
                 list.add(new IfToken());
+                inputPos += 2;
             }
         }
     }
