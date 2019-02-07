1373eb269c86d7e868faf71ba8a702dba5285669
Kyle Dewey <kyle.dewey@csun.edu>
2019-02-07 13:22:17 -0800

    now skips whitespace; uses instance variables for inputPos and input

--- Tokenizer.java
+++ Tokenizer.java
@@ -2,15 +2,43 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Tokenizer {
-    //public static Optional<VariableToken>
-    public static VariableToken tryTokenizeVariable(char[] input,
-                                                    int inputPos) {
+    // begin instance variables
+    private char[] input;
+    private int inputPos;
+    // end instance variables
+    
+    public Tokenizer(final char[] input) {
+        this.input = input;
+        inputPos = 0;
+    }
+
+    private NumberToken tryTokenizeNumber() {
+        String digits = "";
+
+        while (inputPos < input.length &&
+               Character.isDigit(input[inputPos])) {
+            digits += input[inputPos];
+            inputPos++;
+        }
+
+        if (digits.length() > 0) {
+            return new NumberToken(Integer.parseInt(digits));
+        } else {
+            return null;
+        }
+    }
+    
+    private VariableToken tryTokenizeVariable() {
+        final int initialInputPos = inputPos;
         String name = "";
 
+        // variables start with letters
         if (Character.isLetter(input[inputPos])) {
             name += input[inputPos];
             inputPos++;
-            
+
+            // variables can have letters or digits after the first
+            // character
             while (inputPos < input.length &&
                    Character.isLetterOrDigit(input[inputPos])) {
                 name += input[inputPos];
@@ -21,24 +49,52 @@ public class Tokenizer {
         }
 
         if (name.equals("if")) {
+            // reset input position
+            inputPos = initialInputPos;
             return null;
         } else {
             return new VariableToken(name);
         }
     }
-        
-    public static List<Token> tokenize(char[] input) {
+
+    private void skipWhitespace() {
+        while (inputPos < input.length &&
+               Character.isWhitespace(input[inputPos])) {
+            inputPos++;
+        }
+    }
+    
+    private Token tokenizeOne() throws TokenizerException {
+        VariableToken var = null;
+        NumberToken num = null;
+
+        if ((var = tryTokenizeVariable()) != null) {
+            return var;
+        } else if ((num = tryTokenizeNumber()) != null) {
+            return num;
+        } else if (input[inputPos] == '(') {
+            inputPos++;
+            return new LeftParenToken();
+        } else if (input[inputPos] == 'i' &&
+                   input[inputPos + 1] == 'f') {
+            inputPos += 2;
+            return new IfToken();
+        } else {
+            throw new TokenizerException("Invalid character: " +
+                                         input[inputPos] + " at pos " +
+                                         inputPos);
+        }
+    }
+    
+    private List<Token> tokenize() throws TokenizerException {
         List<Token> list = new ArrayList<Token>();
-        int inputPos = 0;
 
         while (inputPos < input.size) {
-            if (input[inputPos] == '(') {
-                list.add(new LeftParenToken());
-                inputPos++;
-            } else if (input[inputPos] == 'i' &&
-                       input[inputPos + 1] == 'f') {
-                list.add(new IfToken());
-                inputPos += 2;
+            skipWhitespace();
+
+            if (inputPos < input.size) {
+                final Token current = tokenizeOne();
+                list.add(current);
             }
         }
     }
