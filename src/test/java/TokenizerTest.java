f8f0cd18b3856c547670e40aa4868bdd719b0947
Kyle Dewey <kyle.dewey@csun.edu>
2019-02-06 17:24:38 -0800

    added tests for tokenizing integers; failing

---
+++ src/test/java/TokenizerTest.java
@@ -0,0 +1,51 @@
+package syntax_example;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+public class TokenizerTest {
+    // specify null for expected if it's not supposed to tokenize
+    public void assertTokenizes(final String input,
+                                final Token[] expected) {
+        final Tokenizer tokenizer = new Tokenizer(input.toCharArray());
+        try {
+            final List<Token> received = tokenizer.tokenize();
+            assertTrue("Expected tokenization failure, got: " + received,
+                       expected != null);
+            assertArrayEquals(expected,
+                              received.toArray(new Token[received.size()]));
+        } catch (final TokenizerException e) {
+            assertTrue("Unexpected tokenization failure for " + input,
+                       expected == null);
+        }
+    }
+
+    @Test
+    public void testTokenizeInteger() {
+        assertTokenizes("123",
+                        new Token[]{ new NumberToken(123) });
+    }
+
+    @Test
+    public void testTokenizeIntegerLeadingWhitespace() {
+        assertTokenizes("  123",
+                        new Token[]{ new NumberToken(123) });
+    }
+
+    @Test
+    public void testTokenizeIntegerTrailingWhitespace() {
+        assertTokenizes("123   ",
+                        new Token[]{ new NumberToken(123) });
+    }
+
+    @Test
+    public void testTokenizeIntegerLeadingAndTrailingWhitespace() {
+        assertTokenizes("  123  ",
+                        new Token[]{ new NumberToken(123) });
+    }
+}
+
+            
